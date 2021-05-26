b9285358efdb4c136c2571392b579dd387cc954d
Maycon Aguiar <maycon.aguiar@gmail.com>
2021-05-26 14:51:35 -0300

    criação da tabela, atualização do compose, criação de script de checagem, finalização do container do banco

---
+++ scripts/init.sql
@@ -0,0 +1,12 @@
+/* cria o banco de dados */
+create database email_sender;
+
+/* conecta ao banco de dados */
+\c email_sender
+
+create table emails (
+    id serial not null ,
+    data timestamp not null default current_timestamp,
+    assunto varchar(100) not null,
+    mensagem varchar(250) not null
+)
